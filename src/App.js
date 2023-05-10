5620a813c737c1458068a57b8cc8cc579417217e
Rafael <rafaelfreire0@gmail.com>
2023-05-10 14:32:12 -0300

    fix
    fix

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BsYXRhZm9ybWEuYXBpYnJhc2lsLmNvbS5ici9hdXRoL2xvZ2luIiwiaWF0IjoxNjc2NDM0ODAzLCJleHAiOjE3MDc5NzA4MDMsIm5iZiI6MTY3NjQzNDgwMywianRpIjoiM0xGSkt3UVVrRkpVODBTdyIsInN1YiI6IjQ1MiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.IGTWpohJfJCr7j7T2x5kzdsFRuV-eTqCrw6dwZDxsko',
           'SecretKey': 'e3b0e4b8-7670-47b6-8543-47f869ccc90e',
-          'PublicToken': 'e101ed3e-f52b-4214-9fd0-a755cbc1f733',
+          'PublicToken': 'Gratis-LJZyLJzCGpLzGlZx253Z',
           'DeviceToken': 'cc8ae6f5-bb1c-4a24-b10c-a5bcbd159961'
         }
       }).then((res) => {
